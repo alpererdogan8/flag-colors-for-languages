983533faed44d35b176994d47f4c7e03870d188d
Alper Erdogan <alpererdogan8@gmail.com>
2023-11-19 22:13:44 +0300

    feat:new init files

---
+++ index.ts
@@ -0,0 +1,18 @@
+type HEXCODE<T extends string> = `#${T}`;
+const SoftwareColors: Readonly<Record<string, HEXCODE<string>>> = {
+  TypeScript: "#3178C6",
+  JavaScript: "#F1E05A",
+  HTML: "#E34C26",
+  CSS: "#E34C26",
+  React: "#149ECA",
+  Vue: "#41B883",
+  Angular: "#CB27BB",
+  Ruby: "#178600",
+  Python: "#3572A5",
+  Shell: "#89E051",
+  DockerFile: "#384D54",
+  C_Sharp: "#178600",
+  C_Plus_Plus: "#F34B7D",
+  Java: "#178600",
+};
+export default SoftwareColors;
